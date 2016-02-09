b4d2fc5a941bce81877201ed1bd2866ca0aa6194
Evaderei <Evaderei@gmail.com>
2016-02-08 20:35:57 -0800

    Added bonfire.
    Added bonfire steam roller

--- Bonfire_Steam_Roller.js
+++ Bonfire_Steam_Roller.js
@@ -1,18 +1,22 @@
-var newArr = [];
 function steamroller(arr) {
   // I'm a steamroller, baby
-  
-  for (var i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) {
-      steamroller(arr[i]);
+  var newArr = [];
+    function rollingPin(arr) {
+      for (var i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+          rollingPin(arr[i]);
+                    console.log(arr[i] + " is being rerolled as a function.");
+
+        }
+        else {
+           newArr.push(arr[i]);
+          console.log(arr[i] + " is pushed to newArr[]");
+        }
+      }
     }
-    else {
-       newArr.push(arr[i]);
-    }
-  }
-  
-  console.log(newArr);
-  return newArr;
+  rollingPin(arr);
+    
+    return newArr;
 }
 
 steamroller([1, [2], [3, [[4]]]]);
